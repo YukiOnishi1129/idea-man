93792ff062fb4f30621c75c73b08f5630119e34a
大西雄貴 <yuki.onishi1129@gmail.com>
2020-08-30 01:51:32 +0900

    アイデアエリアのコンポーネント作成

--- src/components/IdeaArea.tsx
+++ src/components/IdeaArea.tsx
@@ -1,14 +1,61 @@
-import React, { FC } from 'react'
+import React, { FC, useContext } from 'react'
 import styled from 'styled-components'
+import IdeaContext from '../contexts'
 
-export const IdeaField = styled.div`
+export type TProps = {
+  radio: boolean
+}
+
+export const Field = styled.div`
+  display: flex;
+  justify-content: space-around;
   margin: 20px auto;
-  width: 80%;
-  border: 1px solid red;
+  width: ${({ radio }: TProps) => (radio ? '60%' : '90%')};
+`
+
+export const Box = styled.div`
+  width: ${({ radio }: TProps) => (radio ? '45%' : '30%')};
+  height: 200px;
+`
+export const Word = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 10px;
+  box-sizing: border-box;
+  border: 1px solid;
+  background: #f5fffa;
+`
+
+export const MultiIcon = styled.div`
+  width: 20%;
+  height: 100%;
+  line-height: 200px;
+  text-align: center;
+  font-size: 40px;
+  font-weight: bold;
 `
 
 const IdeaArea: FC = () => {
-  return <IdeaField>sss</IdeaField>
+  const { ideas } = useContext(IdeaContext)
+  return (
+    <Field radio={ideas.radio}>
+      <Box radio={ideas.radio}>
+        <Word />
+      </Box>
+      <MultiIcon>×</MultiIcon>
+      <Box radio={ideas.radio}>
+        <Word />
+      </Box>
+      {!ideas.radio && (
+        <>
+          <MultiIcon>×</MultiIcon>
+          <Box radio={ideas.radio}>
+            <Word />
+          </Box>
+        </>
+      )}
+    </Field>
+  )
 }
 
 export default IdeaArea
